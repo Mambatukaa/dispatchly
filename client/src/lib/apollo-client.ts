e9e957a12d95d64700e7e36a1702d23cfd9ce2b0
mambatukaa <sbatuka9@gmail.com>
2026-02-25 11:53:22 -0600

    up

--- client/src/lib/apollo-client.ts
+++ client/src/lib/apollo-client.ts
@@ -2,7 +2,9 @@ import { getToken } from '@/utils/auth'
 import { ApolloClient, ApolloLink, HttpLink, InMemoryCache } from '@apollo/client'
 
 const httpLink = new HttpLink({
-  uri: process.env.NEXT_PUBLIC_GRAPHQL_URL || 'http://localhost:4000/graphql',
+  uri:
+    process.env.NEXT_PUBLIC_GRAPHQL_URL ||
+    'https://dispatchly-backend-a5hye2gve5aae5at.centralus-01.azurewebsites.net/graphql',
   credentials: 'include',
 })
 
